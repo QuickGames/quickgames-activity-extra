33d100003e3c82d2222f5e2ad17da9ba64b68068
quickgames <quickgamesdev@gmail.com>
2018-04-29 22:47:40 +0300

    Create repository: - Added activity params; - Added field id; - Added field preferences get and put.

---
+++ quickgames-activity-extra/src/main/java/quickgames/activity/extra/annotation/FieldId.java
@@ -0,0 +1,19 @@
+package quickgames.activity.extra.annotation;
+
+import android.support.annotation.IdRes;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface FieldId {
+
+    /**
+     * ID for view.
+     */
+    @IdRes int value();
+
+}
---
+++ quickgames-activity-extra/src/main/java/quickgames/activity/extra/annotation/FieldType.java
@@ -0,0 +1,12 @@
+package quickgames.activity.extra.annotation;
+
+/**
+ * Created by quickgames on 29.04.18 20:25.
+ */
+public enum FieldType {
+    STRING,
+    INTEGER,
+    LONG,
+    FLOAT,
+    BOOLEAN
+}
